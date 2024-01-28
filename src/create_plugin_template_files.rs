404de9978b7bb242bdb5b5b61c4dafdb3153d492
c.lamboo <casperlamboo@gmail.com>
2024-01-28 19:16:56 +0100

    Fix packaging

--- src/create_plugin_template_files.rs
+++ src/create_plugin_template_files.rs
@@ -13,10 +13,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         patch = env!("CARGO_PKG_VERSION_PATCH").to_string(),
     );
 
-    let plugin_json_jinja = fs::read_to_string("../templates/plugin.json.jinja")?;
+    let plugin_json_jinja = fs::read_to_string("templates/plugin.json.jinja")?;
     env.add_template("plugin.json", plugin_json_jinja.as_str())?;
 
-    let constants_py_jinja = fs::read_to_string("../templates/constants.py.jinja")?;
+    let package_json_jinja = fs::read_to_string("templates/package.json.jinja")?;
+    env.add_template("package.json", package_json_jinja.as_str())?;
+
+    let constants_py_jinja = fs::read_to_string("templates/constants.py.jinja")?;
     env.add_template("constants.py", constants_py_jinja.as_str())?;
 
     env.get_template("plugin.json")?.render_to_write(
@@ -26,7 +29,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             version => env!("CARGO_PKG_VERSION").to_string(),
             description => env!("CARGO_PKG_DESCRIPTION").to_string(),
         },
-        &mut fs::File::create("../valve/plugin.json")?,
+        &mut fs::File::create("valve/plugin.json")?,
+    )?;
+
+    env.get_template("package.json")?.render_to_write(
+        context! {
+            author => env!("CARGO_PKG_AUTHORS").to_string(),
+            homepage => env!("CARGO_PKG_HOMEPAGE").to_string(),
+            repository => env!("CARGO_PKG_REPOSITORY").to_string(),
+            name => env!("CARGO_PKG_NAME").to_string(),
+            version => env!("CARGO_PKG_VERSION").to_string(),
+            description => env!("CARGO_PKG_DESCRIPTION").to_string(),
+        },
+        &mut fs::File::create("valve/package.json")?,
     )?;
 
     env.get_template("constants.py")?.render_to_write(
@@ -35,7 +50,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             version => env!("CARGO_PKG_VERSION").to_string(),
             settings_prefix => settings_prefix,
         },
-        &mut fs::File::create("../valve/constants.py")?,
+        &mut fs::File::create("valve/constants.py")?,
     )?;
 
     Ok(())
